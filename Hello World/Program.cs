395e6e33948fe6489d27e62e94ef4d1ccdec67d9
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2020-05-25 09:54:48 +0200

    asdfadf

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -5,9 +5,12 @@ namespace Hello_World
 {
     class Program
     {
+        //This is a comment line
+        //This is a comment line2
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); //This is a comment line
+
         }
     }
 }
