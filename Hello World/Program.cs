e4b8f6f5475caa4b43020a2e4f9794ff03f0107f
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2020-05-25 09:37:23 +0200

    First Commit

---
+++ Hello World/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+//This is a comment line
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
